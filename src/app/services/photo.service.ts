1402559e10303c94a119dbb74c9450c6a2dd3316
Linda Vong <lindav@gokic.org>
2020-12-23 22:38:16 -0800

    store all photos captured in array

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -9,6 +9,8 @@ const { Camera, Filesystem, Storage } = Plugins;
 })
 export class PhotoService {
 
+  public photos: Photo[] = []; // array of references to all photos captured
+
   constructor() { }
 
   // Open device camera
@@ -19,5 +21,16 @@ export class PhotoService {
       source: CameraSource.Camera, 
       quality: 100 
     });
+    
+    // Add newly captured photo to beginning of photos array
+    this.photos.unshift({
+      filepath: "soon...",
+      webviewPath: capturedPhoto.webPath})
   }
+}
+
+// Photo interface for holding photo metadata
+export interface Photo {
+  filepath: string;
+  webviewPath: string;
 }
