71f50bd962379cf15dd971f81f880214bf75cf1f
Linda Vong <lindav@gokic.org>
2020-12-23 22:53:22 -0800

    use capacitor filesystem api to save each captured photo to filesystem

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -13,7 +13,7 @@ export class PhotoService {
 
   constructor() { }
 
-  // Open device camera
+  // Open device camera and add image captured to begininng of photos array
   public async addNewToGallery() {
     // Take a photo
     const capturedPhoto = await Camera.getPhoto({
@@ -22,11 +22,49 @@ export class PhotoService {
       quality: 100 
     });
     
-    // Add newly captured photo to beginning of photos array
-    this.photos.unshift({
-      filepath: "soon...",
-      webviewPath: capturedPhoto.webPath})
+    // Save and add newly captured photo to beginning of photos array
+    const savedImageFile = await this.savePicture(capturedPhoto);
+    this.photos.unshift(savedImageFile);
   }
+
+  // Save specified cameraPhoto
+  private async savePicture(cameraPhoto: CameraPhoto) {
+    // Convert photo to base64 format, required by Filesystem API to save
+    const base64Data = await this.readAsBase64(cameraPhoto);
+
+    // Write the file to the data directory
+    const fileName = new Date().getTime() + '.jpeg';
+    const savedFile = await Filesystem.writeFile({
+      path: fileName,
+      data: base64Data,
+      directory: FilesystemDirectory.Data
+    });
+
+    // Use webPath to display the new image instead of base64 since it's
+    // already loaded into memory
+    return {
+      filepath: fileName,
+      webviewPath: cameraPhoto.webPath
+    };
+ }
+
+  // Convert cameraPhoto into base64 format
+  private async readAsBase64(cameraPhoto: CameraPhoto) {
+    // Fetch the photo, read as a blob, then convert to base64 format
+    const response = await fetch(cameraPhoto.webPath!);
+    const blob = await response.blob();
+  
+    return await this.convertBlobToBase64(blob) as string;  
+  }
+  
+  convertBlobToBase64 = (blob: Blob) => new Promise((resolve, reject) => {
+    const reader = new FileReader;
+    reader.onerror = reject;
+    reader.onload = () => {
+        resolve(reader.result);
+    };
+    reader.readAsDataURL(blob);
+  });
 }
 
 // Photo interface for holding photo metadata
